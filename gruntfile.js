09425d1cc38974d95d92518a421da0a721075caa
葫芦娃 <huwenhua@qiaobutang.com>
2016-09-13 20:17:52 +0800

    update

--- gruntfile.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (grunt) {
-    grunt.initConfig({});
-
-    grunt.registerTask('build', 'build the whold project', function () {
-        grunt.log.writeln('build ok').ok();
-    });
-};
